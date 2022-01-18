da036bc4e2b0c8af275967f37651263995e64f3f
Samuel Martins <sbm.martins@gmail.com>
2022-01-18 09:10:13 -0300

    adding a skeleton of our proposal app (segmentation by a seed competition algorith)

---
+++ seed_competition_app.py
@@ -0,0 +1,25 @@
+from magicgui import magicgui
+import napari
+from napari.types import LabelsData, ImageData
+
+from skimage.io import imread
+
+
+@magicgui(call_button='Run Threshold',
+          threshold={"widget_type": "Slider", "min": 0, "max": 255},
+)
+def threshold_segmentation(img: ImageData, threshold: int = 100) -> LabelsData:
+    """Threshold an image and return a mask."""
+    labels = (img >= threshold).astype(int)
+
+    return labels
+
+
+with napari.gui_qt():
+    img = imread('./data/brain_snapshot.png')
+    
+    viewer = napari.Viewer()
+    viewer.add_image(img)
+
+    viewer.window.add_dock_widget(threshold_segmentation)
+
