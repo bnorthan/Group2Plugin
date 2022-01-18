b150d95dcc4a1afeee093353cf050e4995d3c7b7
Yannan-Chen <yc3338@columbia.edu>
2022-01-18 08:31:39 -0500

    Detection of no images loaded

--- seed_competition_app.py
+++ seed_competition_app.py
@@ -17,12 +17,15 @@ def segment_by_seed_competition(img: ImageData, markers: LabelsData) -> LayerDat
     # gray 2D image = (width, height)
     # color 2D image = (width, height, n_channels=3)
     # 3D image = (width, height, slices)
+    if (img is None) or (markers is None):
+        return
+    
     is_3D = (img.ndim == 3) and (img.shape[-1] > 3)
     print(f'Is a 3D image: {is_3D}')
 
     print('#### Running Segmentation by IFT Seed Competition')
-    _, _, _, labels, = seed_competition(markers, img, image_3d=is_3D)
-
+#     _, _, _, labels, = seed_competition(markers, img, image_3d=is_3D)
+    print('Done')
     # remove background
     labels[labels == 1] = 0
 
